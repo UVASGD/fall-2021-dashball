46197e47b2f6eb016f120df233232486c99b8b60
Chris Ricigliano <cmr4jw@virginia.edu>
2021-09-23 01:26:48 -0400

    simple firing
    added simple projectile firing to enemy

---
+++ Assets/Scripts/Enemy/bulletSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSpawn : MonoBehaviour
+{
+ public GameObject bullet;
+ public Transform spawnPoint;
+
+ void Update ()
+ {
+    //check to see if shoot
+    //getbutton down used for testing (right click)
+     if (Input.GetButtonDown("Fire2")) 
+     Shoot();
+     
+  
+ }
+
+    void Shoot()
+    {
+        //fire bullet from fire point
+    Instantiate(bullet,spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
