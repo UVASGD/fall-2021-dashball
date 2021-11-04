82a9d35736d4eb6c4bf225e9d51f69fe34794e2d
Chris Ricigliano <cmr4jw@virginia.edu>
2021-11-04 13:19:40 -0400

    prevent player from leaving boss zone

---
+++ Assets/Scripts/Enemy/playerBoundsBoss.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBoundsBoss : MonoBehaviour
+{
+    public GameObject TheBoss;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "player")
+        {
+            col.gameObject.GetComponent<Rigidbody2D>().velocity = col.gameObject.GetComponent<Rigidbody2D>().velocity * -1;
+        }
+    }
+}
